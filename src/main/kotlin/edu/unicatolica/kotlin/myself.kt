d930283c4605338628f0ed6875f9375b081f191b
Tatiana <tatianamontoya0428@gmail.com>
2025-08-14 21:50:58 -0500

    se agrega archivo miself con mi infomación corregida

--- src/main/kotlin/edu/unicatolica/kotlin/myself.kt
+++ src/main/kotlin/edu/unicatolica/kotlin/myself.kt
@@ -1,15 +1,15 @@
 package edu.unicatolica.kotlin
 
-fun myself() {
-    var nombre: String = "Tatiana Montoya Vilamil"
-    nombre = "String"
-
+fun main() {
+    val nombre = "Tatiana Montoya Vilamil"
     val edad = 22
     val programa = "Tecnología en Desarrollo de Software"
-    val interest = "me apasiona la calidad del software. Cuento con conocimientos básicos en pruebas y metodologías QA, y tengo gran interés en seguir formándome profesionalmente en este campo."
+    val interest = "me apasiona la calidad del software. Cuento con conocimientos básicos en pruebas y metodologías."
 
     val message = "Hola, mi nombre es $nombre, tengo $edad años, soy del programa de $programa y $interest."
 
     println(message)
-
 }
+
+
+
