16fa3340d2d5476052beeda2ef4f4400bb2e3471
Tatiana <tatianamontoya0428@gmail.com>
2025-08-14 21:41:43 -0500

    se agrega archivo miself con mi infomación

--- src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
+++ src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
@@ -1,6 +1,32 @@
 // Variables&Types.kt
 package edu.unicatolica.kotlin
 
+
 fun main() {
     // variables and data types
+    println ("Hello Word")
+
+    var name: String = "Tatiana"
+    name = "String"
+
+    val number1: Int = 42
+    val number2: Long = 3L
+    val number3: Float = 3.14f
+    val number4: Double = 2.3
+
+    val adding = 10 + 1
+    val substract = 10 - 1
+    val product = 10 * 2
+    val division = 10 / 1
+    val moduling = 10 % 3
+
+    val name3 = "Lina"
+    val age = 19
+    val city= "palmira"
+
+    val message = "Hi, $name3. Your age is $age and I am $city"
+
+    println(message)
+
+
 }
---
+++ src/main/kotlin/edu/unicatolica/kotlin/myself.kt
@@ -0,0 +1,15 @@
+package edu.unicatolica.kotlin
+
+fun myself() {
+    var nombre: String = "Tatiana Montoya Vilamil"
+    nombre = "String"
+
+    val edad = 22
+    val programa = "Tecnología en Desarrollo de Software"
+    val interest = "me apasiona la calidad del software. Cuento con conocimientos básicos en pruebas y metodologías QA, y tengo gran interés en seguir formándome profesionalmente en este campo."
+
+    val message = "Hola, mi nombre es $nombre, tengo $edad años, soy del programa de $programa y $interest."
+
+    println(message)
+
+}
